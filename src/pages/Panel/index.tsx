439b86b002cd1644b866486b610bc9f347986d8c
YukiYama <zx275fire@gmail.com>
2023-01-11 17:04:31 +0900

    refactor: change js to ts

--- src/pages/Panel/index.tsx
+++ src/pages/Panel/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
 
 import Panel from './Panel';
@@ -6,4 +6,5 @@ import './index.css';
 
 render(<Panel />, window.document.querySelector('#app-container'));
 
+// @ts-ignore
 if (module.hot) module.hot.accept();
