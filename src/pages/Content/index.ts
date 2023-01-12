6eb2ae5083832f95ec130a87ad4f4e032e25cad5
YukiYama <zx275fire@gmail.com>
2023-01-12 21:40:12 +0900

    add: add functionality of change style for half-window YouTube Live

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,6 +1,12 @@
 import { printLine } from './modules/print';
+import { styleForHalfWindow } from './modules/styleForHalfWindow';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'x' && evt.ctrlKey) {
+    styleForHalfWindow();
+  }
+});
