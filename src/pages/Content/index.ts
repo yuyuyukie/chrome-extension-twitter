e134c8774accd106d142ec90e1629fc92fb7ac08
YukiYama <zx275fire@gmail.com>
2023-01-23 13:17:22 +0900

    feat: click following func

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,15 +1,18 @@
-import { printLine } from './modules/print';
+import {printLine} from './modules/print';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
 
-document.addEventListener('DOMContentLoaded', () => {
+const clickFollowing = () => {
   const followingButton = document.querySelector(
-    "div[data-testid='ScrollSnap-List'] > div:last-child"
+    "div[data-testid='ScrollSnap-List'] > div:last-child > a"
   );
   if (followingButton instanceof HTMLElement) {
     followingButton.click();
+    clearInterval(interval)
   }
-});
+}
+
+let interval = setInterval(clickFollowing, 1000)
