d34105b25ac5f0a0a8e1c51794e91504053934bd
YukiYama <zx275fire@gmail.com>
2023-01-23 13:01:26 +0900

    chore: migrate to twitter

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,12 +1,15 @@
 import { printLine } from './modules/print';
-import { styleForHalfWindow } from './modules/styleForHalfWindow';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'x' && evt.ctrlKey) {
-    styleForHalfWindow();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const followingButton = document.querySelector(
+    "div[data-testid='ScrollSnap-List'] > div:last-child"
+  );
+  if (followingButton instanceof HTMLElement) {
+    followingButton.click();
   }
 });
